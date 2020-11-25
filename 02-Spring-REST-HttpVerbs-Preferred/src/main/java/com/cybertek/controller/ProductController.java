ee763bb92949fd4aecf5203a5243a7d64422fe81
karamanmehmet <karamanmehmet@yahoo.com>
2020-11-24 20:12:00 -0500

    -basepath -request mapping enhancements

--- 02-Spring-REST-HttpVerbs-Preferred/src/main/java/com/cybertek/controller/ProductController.java
+++ 02-Spring-REST-HttpVerbs-Preferred/src/main/java/com/cybertek/controller/ProductController.java
@@ -3,17 +3,20 @@ package com.cybertek.controller;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cybertek.model.Product;
 import com.cybertek.service.ProductService;
 
-@Controller
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
 	private ProductService productService;
@@ -23,34 +26,32 @@ public class ProductController {
 		this.productService = productService;
 	}
 
-	@RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
-	public @ResponseBody Set<Product> delete(@PathVariable("id") Long id) {
+	@DeleteMapping("/{id}")
+	public Set<Product> delete(@PathVariable("id") Long id) {
 		Set<Product> set = productService.delete(id);
 		return set;
 	}
 
-	@RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
-	public @ResponseBody Set<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+	@PutMapping("/{id}")
+	public Set<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
 		Set<Product> set = productService.updateProduct(id, product);
 		return set;
 	}
 
-	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	@PostMapping
 	public Set<Product> createProduct(@RequestBody Product product) {
 		Set<Product> set = productService.createProduct(product);
 		return set;
 	}
 
-	@RequestMapping(value = "/products")
-	public @ResponseBody Set<Product> getProducts() {
+	@GetMapping
+	public Set<Product> getProducts() {
 		return productService.getProducts();
 	}
 
-	@RequestMapping(value = "/products/{id}")
-	public @ResponseBody Product getProduct(@PathVariable("id") Long id) {
+	@GetMapping(value = "/{id}")
+	public Product getProduct(@PathVariable("id") Long id) {
 		return productService.getProduct(id);
 	}
-	
-
 
 }
